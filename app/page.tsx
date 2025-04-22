0db66d016a4702ffdd91af7415e566aff62f7872
Sahil Pate <sahilpate7@gmail.com>
2025-04-22 21:47:23 +0530

    tailwind configured

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-
 const Home =()=> (
-   <h1 className={"text-3xl text-violet-600 font-black"}>welcome to next js</h1>
+   <h1 className={"text-3xl text-primary-500 font-black"}>welcome to next js</h1>
   );
-
 
 export default Home;
